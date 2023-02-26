0865da73ce7cf4e845eb7afdefe20217595e4bff
anaBeatrizWalker <anabiawalker@gmail.com>
2023-02-26 15:13:33 -0300

    POO: Interface

---
+++ src/edu/anabeatriz/java/orientacaoAObjetos/interfaces/OperacoesMatematicas.java
@@ -0,0 +1,13 @@
+package edu.anabeatriz.java.orientacaoAObjetos.interfaces;
+
+public interface OperacoesMatematicas {
+
+    //Métodos sem corpo
+    void soma(double operando1, double operando2);
+
+    void subtracao(double operando1, double operando2);
+
+    void multiplicacao(double operando1, double operando2);
+
+    void divisao(double operando1, double operando2);
+}
