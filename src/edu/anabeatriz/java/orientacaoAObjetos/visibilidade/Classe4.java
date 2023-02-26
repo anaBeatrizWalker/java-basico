a97e8cc334bae906d02e51a071aee9a50b97c4dd
anaBeatrizWalker <anabiawalker@gmail.com>
2023-02-26 15:30:04 -0300

    POO: Visibilidade

---
+++ src/edu/anabeatriz/java/orientacaoAObjetos/visibilidade/Classe4.java
@@ -0,0 +1,14 @@
+package edu.anabeatriz.java.orientacaoAObjetos.visibilidade;
+
+import edu.anabeatriz.java.orientacaoAObjetos.visibilidade.pacote.Classe1;
+
+public class Classe4 {
+    Classe1 classe1;
+
+    void metodo() {
+
+        //atributo3
+
+        //metodo3
+    }
+}
---
+++ src/edu/anabeatriz/java/orientacaoAObjetos/visibilidade/pacote/Classe1.java
@@ -0,0 +1,21 @@
+package edu.anabeatriz.java.orientacaoAObjetos.visibilidade.pacote;
+
+public class Classe1 {
+    private String atributo1;
+
+    protected String atributo2;
+
+    public String atributo3;
+
+    private void metodo1() {
+
+    }
+
+    protected void metodo2() {
+
+    }
+
+    public void metodo3() {
+
+    }
+}
