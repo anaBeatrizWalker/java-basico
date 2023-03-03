0793467a8afb919ec5e2f95c972727c4ce89f099
anaBeatrizWalker <anabiawalker@gmail.com>
2023-03-03 14:33:09 -0300

    Tratamento de Exceção Customizada (parte 2)

--- src/edu/anabeatriz/java/excecoes/ExceptionCustomizada2.java
+++ src/edu/anabeatriz/java/excecoes/ExceptionCustomizada2.java
@@ -1,5 +1,7 @@
 package edu.anabeatriz.java.excecoes;
 
+import javax.swing.JOptionPane;
+
 public class ExceptionCustomizada2 {
     public static void main(String[] args) {
         
@@ -7,14 +9,31 @@ public class ExceptionCustomizada2 {
         int[] denominador = {2, 4, 0, 2, 8};
 
         for(int i = 0; i < denominador.length; i++){
-            int resultado = numerador[i] / denominador[i];
-            System.out.println(resultado);
+            try {
+                if(numerador[i] % 2 != 0) 
+                    throw new DivisaoNaoExataException("Divisão não exata!", numerador[i], denominador[i]);
+
+                int resultado = numerador[i] / denominador[i];
+                System.out.println(resultado);
+
+            } catch (DivisaoNaoExataException e) {
+                e.printStackTrace(); 
+                //edu.anabeatriz.java.excecoes.DivisaoNaoExataException: Divisão não exata!
+                    //at edu.anabeatriz.java.excecoes.ExceptionCustomizada2.main(ExceptionCustomizada2.java:14)
+                JOptionPane.showMessageDialog(null, e.getMessage());
+            }
         }
     }
-    /*
-    2
-    1
-    Exception in thread "main" java.lang.ArithmeticException: / by zero
-        at edu.anabeatriz.java.excecoes.ExceptionCustomizada2.main(ExceptionCustomizada2.java:10)
-    */
 }
+
+class DivisaoNaoExataException extends Exception {
+    
+    private int numerador;
+    private int denominador;
+
+    public DivisaoNaoExataException(String message, int numerador, int denominador) {
+        super(message);
+        this.numerador = numerador;
+        this.denominador = denominador;
+    }
+} 
