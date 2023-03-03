f8dc7126aa7177a5b2e2ef01ea086f2122dc4b1a
anaBeatrizWalker <anabiawalker@gmail.com>
2023-03-03 14:17:41 -0300

    Tratamento de Exceção Customizada

--- src/edu/anabeatriz/java/excecoes/ExceptionCustomizada.java
+++ src/edu/anabeatriz/java/excecoes/ExceptionCustomizada.java
@@ -21,13 +21,37 @@ public class ExceptionCustomizada {
                 line = br.readLine();
             } while (line != null);
 
+        } catch (ImpossivelAberturaDeArquivoException e) {
+            JOptionPane.showMessageDialog(null, "Arquivo não encontrado. " + e.getMessage());
+
         } catch (IOException e) {
             JOptionPane.showMessageDialog(null, "Ops! Ocorreu um erro não inesperado." + e.getMessage());
+
         }
     }
 
-    public static BufferedReader lerArquivo(String nomeDoArquivo) {
+    public static BufferedReader lerArquivo(String nomeDoArquivo) throws ImpossivelAberturaDeArquivoException {
         File file = new File(nomeDoArquivo);
-        return new BufferedReader(new FileReader(nomeDoArquivo)); //FileNotFoundException
+        try {
+            return new BufferedReader(new FileReader(nomeDoArquivo));
+        } catch (FileNotFoundException e) {
+            //throw lança nova exception
+            throw new ImpossivelAberturaDeArquivoException(file.getName(), file.getPath());
+        }
+    }
+}
+class ImpossivelAberturaDeArquivoException extends Exception{
+
+    private String nomeDoArquivo;
+    private String caminho;
+
+    public ImpossivelAberturaDeArquivoException(String nomeDoArquivo, String caminho) {
+        super("O arquivo " + nomeDoArquivo + " não foi encontrado no diretório " + caminho);
+        this.nomeDoArquivo = nomeDoArquivo;
+        this.caminho = caminho;
+    }
+    @Override
+    public String toString() {
+        return "ImpossivelAberturaDeArquivoException { nomeDoArquivo=" + nomeDoArquivo + "; diretório=" + caminho + " }";  
     }
 }
